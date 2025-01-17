9abd10da44da792a373b66eac3c7d86df7af1919
Zheng5005 <u.gomezherrera@gmail.com>
2025-01-16 20:17:44 -0600

    Signup and Login Pages
    It needs testing

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -4,20 +4,42 @@ import Signup from "./pages/Signup"
 import Login from "./pages/Login"
 import Settings from "./pages/Settings"
 import Profile from "./pages/Profile"
-import {Routes, Route} from "react-router-dom"
+import {Routes, Route, Navigate} from "react-router-dom"
+import useAuthStore from "./store/useAuthStore"
+import { useEffect } from "react"
+import {Loader} from "lucide-react"
+import { Toaster } from "react-hot-toast"
 
 function App() {
+  const {authUser, checkAuth, isCheckingAuth} = useAuthStore()
+
+  useEffect(() => {
+    checkAuth();
+  }, [checkAuth])
+
+  console.log({authUser})
+
+  if(isCheckingAuth && !authUser){
+    return(
+      <div className="flex items-center justify-center h-screen"> 
+        <Loader className="size-10 animate-spin"/>
+      </div>
+    )
+  }
+
   return (
     <>
       <div>
         <Navbar />
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/login" element={<Login />} />
+          <Route path="/" element={authUser ? <Home /> : <Navigate to="/login"/>} />
+          <Route path="/signup" element={!authUser ? <Signup /> : <Navigate to="/"/>} />
+          <Route path="/login" element={!authUser ? <Login /> : <Navigate to="/"/>} />
           <Route path="/settings" element={<Settings />} />
-          <Route path="/profile" element={<Profile />} />
+          <Route path="/profile" element={authUser ? <Profile /> : <Navigate to="/login"/>} />
         </Routes>
+
+        <Toaster />
       </div>
     </>
   )
