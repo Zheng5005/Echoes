92fb45ad64a6da9f483039d569c3e86739fdb1fc
Zheng5005 <u.gomezherrera@gmail.com>
2025-06-17 10:24:34 -0600

    Getting ready to deploy

--- Server/src/index.js
+++ Server/src/index.js
@@ -2,6 +2,7 @@ import express from "express"
 import authRoutes from "./routes/auth.route.js"
 import messageRoutes from "./routes/message.route.js"
 import dotenv from "dotenv"
+import path from "path";
 import cookieParser from "cookie-parser"
 import cors from "cors"
 import connectDB from "./lib/db.js"
@@ -9,7 +10,8 @@ import { app, server } from "./lib/socket.js"
 
 dotenv.config()
 
-const PORT = process.env.PORT | 3000
+const PORT = process.env.PORT;
+const __dirname = path.resolve();
 
 app.use(express.json())
 app.use(cookieParser())
@@ -20,6 +22,14 @@ app.use(cors({
 
 app.use("/api/auth", authRoutes)
 app.use("/api/messages", messageRoutes)
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../Client/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../Client", "dist", "index.html"));
+  });
+}
 
 server.listen(PORT, () => {
     console.log(`server listening on PORT: ${PORT}`)
